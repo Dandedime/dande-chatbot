d9de854cd0cf63c70557072ab8bcd68153c6c091
Jared Lafer <jaredlafer@macbook-pro-2.mynetworksettings.com>
2024-01-31 17:44:08 -0500

    exception handle

--- conversation.py
+++ conversation.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from openai import OpenAI
 from utils import extract_sql
-from build_prompts import SystemPrompt
+from build_prompts import SQLPrompt
 
 
 class ConversationOpenAI:
@@ -28,27 +28,47 @@ class SQLConversation(ConversationOpenAI):
         user question: {question}
         query: {query}
         results: {results}
-    """
-        self.system_prompt = SystemPrompt(instruction_file=Path("prompt_contexts/sql.txt"), table_files=[Path("table_contexts/violations.txt")]).system_prompt
+        """
+
+        self.error_prompt = """Given the following user question, sql query, and error thrown by running the query, first inform the user
+        there was an error executing the query and then describe the error. Then suggest possible corrections to this query, and if something 
+        in the question or query is unclear ask for further clarification from the user:
+        
+        user question: {question}
+        query: {query}
+        error: {error}
+        """
+        self.error_prompt = '\n'.join([line.strip() for line in self.error_prompt.strip().split('\n')])
+
+        self.system_prompt = SQLPrompt(instruction_file=Path("prompt_contexts/sql.txt"), table_files=[Path("table_contexts/violations.txt")]).system_prompt
 
         self.conversation_history = ConversationHistory(self.system_prompt, recent_window=memory_window)
 
     def write_query(self, messages):
-        """Construct a sql query an explanation given the current conversation"""
+        """Construct a sql query with an explanation given the current conversation"""
         return self.respond(messages)
 
     def execute_query(self, response):
         """Execute the sql query in the response text if there is one"""
         query = extract_sql(response)
         if query is not None:
-            return query, self.db_conn.query(query)
+            try:
+                res = self.db_conn.query(query)
+                return query, res, None
+            except Exception as e:
+                return query, None, e
         else:
-            return None, None
+            return None, None, None
 
     def answer(self, user_message, query, results):
-        """Construct an answer to the question in the user_message give the sql query in results of the query"""
+        """Construct an answer to the question in the user_message given the sql query in results of the query"""
         question = user_message["content"]
         content = self.answer_prompt.format(question=question, query=query, results=results)
+        return self.respond([{"role": "system", "content": content}])
+    
+    def error(self, user_message, query, status):
+        question = user_message["content"]
+        content = self.error_prompt.format(question=question, query=query, error=status)
         return self.respond([{"role": "system", "content": content}])
 
     def _write_response(self, generator, resp_container):
@@ -64,12 +84,21 @@ class SQLConversation(ConversationOpenAI):
         """Create the full response, including query and explanation, results of the query, and the answer to the original question"""
         response, resp_container = self._write_response(self.write_query(self.conversation_history.recent_conversation), resp_container)
         response_msg = {"role": "assistant", "content": response}
-        query, results = self.execute_query(response)
-        if results is not None:
+        query, results, status = self.execute_query(response)
+        
+        if results is not None and not results.empty: 
+            print(1)
             user_message = self.conversation_history.user_messages[-1]
             response_msg["results"] = results
             answer, _ = self._write_response(self.answer(user_message, query, results), resp_container)
-
+        elif results is not None and results.empty:
+           print(2)
+        if status is not None:
+            print(3)
+            user_message = self.conversation_history.user_messages[-1]
+            answer, _ = self._write_response(self.error(user_message, query, status), resp_container)
+            
+            
 
 class ConversationHistory:
     def __init__(self, system_prompt, recent_window=5):
