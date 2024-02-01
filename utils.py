bdfccb57f2811b4771ea2b590fccb2a310af9968
Jared Lafer <jaredlafer@macbook-pro-2.mynetworksettings.com>
2024-02-01 14:04:38 -0500

    include empty error

--- utils.py
+++ utils.py
@@ -5,11 +5,23 @@ def write_full_response(conversation: SQLConversation):
     """Create the full response, including query and explanation, results of the query, and the answer to the original question"""
     query_explanation = conversation.write_query()
     response = write_response(query_explanation, conversation)
-    query, results = conversation.execute_query(response)
-    if results is not None:
+    query, results, status = conversation.execute_query(response)
+
+    if results is not None and not results.empty: 
+        print(1)
         user_message = conversation.history.user_messages[-1]
         st.dataframe(results)
-        _ = write_response(conversation.answer(user_message, query, results), conversation)
+        _ = write_response(conversation.answer(user_message, query, results), st.empty())
+    elif results is not None and results.empty:
+        print(2)
+        user_message = conversation.history.user_messages[-1]
+        st.dataframe(results)
+        _ = write_response(conversation.empty(user_message, query, results), st.empty())
+    if status is not None:
+        print(3)
+        user_message = conversation.history.user_messages[-1]
+        _ = write_response(conversation.error(user_message, query, status), st.empty())
+
 
 def write_response(generator, conversation, results=None):
     """Construct response from the generator and append to conversation history"""
