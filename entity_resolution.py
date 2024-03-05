736f95950f617f7866f87dd41982a6b7dd6c89cd
Ramsey Jones <jones.ramsey@gmail.com>
2024-03-05 10:44:10 -0800

    entity resolution example

--- entity_resolution.py
+++ entity_resolution.py
@@ -1,12 +1,14 @@
 import uuid
+import pinecone
+import streamlit as st
 
-from data import Entity
+from data import Entity, Corporation
 from langchain_openai import OpenAIEmbeddings
 
 
 class EntityResolution:
 
-    def __init__(self, pinecone_index, threshold: float = 0.97, embedding=None):
+    def __init__(self, pinecone_index, threshold: float = 0.9, embedding=None):
         self.pinecone_index = pinecone_index
         if embedding is None:
             embedding = OpenAIEmbeddings()
@@ -14,7 +16,7 @@ class EntityResolution:
 
         self.threshold = threshold
 
-    def resolve(self, entity_data: Entity):
+    def resolve(self, entity_data: Entity, upsert: bool = False):
         """
         Args:
             entity_data: Entity class instance containing data for entity to be
@@ -24,17 +26,36 @@ class EntityResolution:
                 upserted entity
         """
         data_str = entity_data.to_text()
-        vector = self.embed_query(data_str)
-        best_match_entity = self.pinecone_index.query(vector=vector, top_k=1)
-        if best_match_entity[0]["score"] >= self.threshold:
-            pinecone_id = best_match_entity[0]["id"]
+        vector = self.embedding.embed_query(data_str)
+        best_match_entity = self.pinecone_index.query(vector=vector, top_k=1,
+                                                      include_metadata=True)
+        if best_match_entity["matches"][0]["score"] >= self.threshold:
+            matched_entity = best_match_entity["matches"][0]
+            pinecone_id = matched_entity["id"]
         else:
-            pinecone_id = str(uuid.uuid4().hex())
-            metadata = {"text": data_str}
-            self.pinecone_index.upsert(vectors=[{"id": pinecone_id, "values":
-                                                 vector, "metadata":
-                                                 metadata}])
-        return pinecone_id
+            matched_entity = None
+            if upsert:
+                pinecone_id = str(uuid.uuid4().hex())
+                metadata = {"text": data_str}
+                self.pinecone_index.upsert(vectors=[{"id": pinecone_id, "values":
+                                                     vector, "metadata":
+                                                     metadata}])
+            else:
+                pinecone_id = None
+        return matched_entity, pinecone_id
 
     #potentially some llm verificatino stuff?
 
+
+if __name__ == "__main__":
+    pc = pinecone.Pinecone(api_key=st.secrets.PINECONE_API_KEY)
+    index = pc.Index("entities")
+    er = EntityResolution(index)
+
+    hca = Corporation(name="HCA Healthcare", industry="healtchare services")
+    matched, _ = er.resolve(hca)
+    print(matched)
+    mmm =  Corporation(name="3M", industry="salvation",
+                          ownership_structure="public", hq_state="MN")
+    matched, _ = er.resolve(mmm)
+    print(matched)
