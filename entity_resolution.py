c9c0d30271e9d45dd92e1d29c8ce8f613c999869
Ramsey Jones <jones.ramsey@gmail.com>
2024-03-05 09:42:40 -0800

    snowflake cursor batches

--- entity_resolution.py
+++ entity_resolution.py
@@ -1,15 +1,18 @@
+import uuid
+
 from data import Entity
+from langchain_openai import OpenAIEmbeddings
 
 
 class EntityResolution:
 
-    def __init__(self, pinecone_index, embedding=None):
+    def __init__(self, pinecone_index, threshold: float = 0.97, embedding=None):
         self.pinecone_index = pinecone_index
         if embedding is None:
-            embedding = OpenAIEmbedding()
+            embedding = OpenAIEmbeddings()
         self.embedding = embedding
 
-        # threshold and stuff
+        self.threshold = threshold
 
     def resolve(self, entity_data: Entity):
         """
@@ -20,19 +23,18 @@ class EntityResolution:
             Pinecone id either referring to an existing entity or for a newly
                 upserted entity
         """
-        data_str = self._format_data_str(entity_data)
-        vector = self.embed(data_str)
+        data_str = entity_data.to_text()
+        vector = self.embed_query(data_str)
         best_match_entity = self.pinecone_index.query(vector=vector, top_k=1)
+        if best_match_entity[0]["score"] >= self.threshold:
+            pinecone_id = best_match_entity[0]["id"]
+        else:
+            pinecone_id = str(uuid.uuid4().hex())
+            metadata = {"text": data_str}
+            self.pinecone_index.upsert(vectors=[{"id": pinecone_id, "values":
+                                                 vector, "metadata":
+                                                 metadata}])
+        return pinecone_id
 
-    @staticmethod
-    def _format_data_str(entity_data):
-        data_str = f"{entity_data.entity_type} named {entity_data.name} with "
-        
-        field_strs = [f"{field} of {value}" for field, value in
-                      asdict(entity_data).items() if field not in
-                      ["entity_type", "name"]]
-        data_str += ", ".join(field_strs)
-        return data_str
+    #potentially some llm verificatino stuff?
 
-
-    #potentially some llm verificatino stuff
