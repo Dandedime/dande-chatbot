e9cc0b132de4bcaf5dadf512d10bdeea2bad484a
Jared Lafer <jaredlafer@MacBook-Pro-3.local>
2024-03-13 16:12:11 -0400

    gpt entity res

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from rag import PineconeIndex
-from conversation import SQLConversation
+from conversation import SQLConversation, PineconeConversation
 import string
+from typing import List
 
 def write_full_response(conversation: SQLConversation, table_selection = None):
     """Create the full response, including query and explanation, results of the query, and the answer to the original question"""
@@ -30,6 +31,14 @@ def select_tables(conversation: SQLConversation):
     response = [x.lstrip(" ").lstrip(".").rstrip(".") for x in response.split(",")]
     return [s.translate(str.maketrans('', '', ",")) for s in response]
 
+def find_pinecone_match(conversation: PineconeConversation, query: str, candidates: List[str]):
+    query_explanation = conversation.find_match(query, candidates)
+    response = ""
+    for chunk in query_explanation:
+        response += (chunk.choices[0].delta.content or "")
+    response = [x for x in response if x in "0123456789"]
+    return "".join(response)
+    
 def write_response(generator, conversation, results=None):
     """Construct response from the generator and append to conversation history"""
     response = ""
